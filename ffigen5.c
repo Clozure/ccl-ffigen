b9f331e726af895d2bd887023edef0c6a0c081d0
Bo Yao <icerove@gmail.com>
2018-01-07 02:06:15 -0500

    figure out a way to check and convert primitive type

--- ffigen5.c
+++ ffigen5.c
@@ -4,36 +4,70 @@
 #include <ctype.h>
 #include <clang-c/Index.h>
 
-void print_indent(unsigned int level)
+typedef struct {
+    const char *clang_name;
+    const char *ffi_name;
+} ffi_primitive_name_map;
+
+static ffi_primitive_name_map ffi_primitive_names[] =
 {
-    unsigned int i;
-    for (i = 0; i < level; i++) {
-        putchar(' ');
-    }
+    {"int", "int"}, 
+    {"char", "char"},
+    {"float", "float"},
+    {"double", "double"},
+    {"long double", "long-double"}, 
+    {"void", "void"},
+    {"long int", "long"}, 
+    {"unsigned int", "unsigned"}, 
+    {"long unsigned int", "unsigned-long"}, 
+    {"long long int", "long-long"}, 
+    {"long long unsigned int", "unsigned-long-long"}, 
+    {"short int", "short"}, 
+    {"short unsigned int", "unsigned-short"}, 
+    {"signed char", "signed-char"}, 
+    {"unsigned char", "unsigned-char"}, 
+    {"complex int", "complex-int"},
+    {"complex float", "complex-float"},
+    {"complex double", "complex-double"},
+    {"complex long double", "complex-long-double"},
+    {"__vector unsigned char", "__vector-unsigned-char"},
+    {"__vector signed char", "__vector-signed-char"},
+    {"__vector bool char", "__vector-bool-char"},
+    {"__vector unsigned short", "__vector-unsigned-short"},
+    {"__vector signed short", "__vector-signed-short"},
+    {"__vector bool short", "__vector-bool-short"},
+    {"__vector unsigned long", "__vector-unsigned-long"},
+    {"__vector signed long", "__vector-signed-long"},
+    {"__vector bool long", "__vector-bool-long"},
+    {"__vector float", "__vector-short-float"},
+    {"__vector pixel", "__vector-pixel"},
+    {"__vector", "__vector"},  
+    {"_Bool", "unsigned"},
+    {"__int128_t", "long-long-long"}, 
+    {"__uint128_t", "unsigned-long-long-long"}, 
+    {NULL, NULL}
+};
+
+/* Return the Lisp'y name for the type GCC_NAME */
+static char *
+to_primitive_type_name (const char* clang_name)
+{
+  ffi_primitive_name_map *map = ffi_primitive_names;
+  for (; map->clang_name; ++map)
+    if (!strcmp (map->clang_name, clang_name))
+      return (char*)map->ffi_name;
+  fprintf(stderr, "Bug: couldn't find primitive name for %s\n", clang_name);
+  exit(-1);
 }
 
-void printCursorKind(enum CXCursorKind cursorKind)
+char * ffi_primitive_type_name (CXCursor decl)
 {
-    CXString kindName = clang_getCursorKindSpelling(cursorKind);
-    printf("%s ", clang_getCString(kindName));
-    clang_disposeString(kindName);
+  CXType type = clang_getCursorType(decl);
+  CXString type_str = clang_getTypeSpelling(type);
+  char * result = to_primitive_type_name(clang_getCString(type_str));
+  clang_disposeString(type_str);
+  return result;
 }
-
-void printCursorSpelling(CXCursor cursor)
-{
-    CXString cursorSpelling = clang_getCursorSpelling(cursor);
-    printf("(%s) ", clang_getCString(cursorSpelling));
-    clang_disposeString(cursorSpelling);
-}
-
-void printCursorType(CXCursor cursor)
-{
-    CXType cursorType = clang_getCursorType(cursor);
-    CXString typeSpelling = clang_getTypeSpelling(cursorType);
-    printf("[%s]\n", clang_getCString(typeSpelling));
-    clang_disposeString(typeSpelling);
-}
-
 
 enum CXChildVisitResult visit_func(CXCursor cursor, CXCursor parent, CXClientData client_data);
 
@@ -139,7 +173,7 @@ void process_macro_definition(CXCursor cursor, CXString filename, unsigned line)
     if (filename_s) {
         char * definition = get_macro_definition(cursor, filename);
         int is_predefined_header = (strcmp(filename_s, PREDEFINED_HEADER_PATH) == 0);
-        printf("(macro (\"%s\" %d) ", 
+        printf("(macro (\"%s\" %u) ", 
             is_predefined_header ? "" : filename_s,
             is_predefined_header ? 1 : line
         );
@@ -171,6 +205,95 @@ void process_enum_constant_decl(CXCursor cursor, CXString ident, int is_inside_e
     }
 }
 
+void format_storage_kind(CXCursor cursor)
+{
+    if (clang_Cursor_getStorageClass(cursor) == CX_SC_Extern) {
+        fputs("(extern)", ffifile);
+    } else {
+        fputs("(static)", ffifile);
+    }
+}
+
+int is_builtin_type(enum CXTypeKind kind)
+{
+    return (CXType_FirstBuiltin <= kind) && (kind <= CXType_LastBuiltin);
+}
+
+int is_primitive_type(enum CXTypeKind kind)
+{
+    return is_builtin_type(kind) ||
+           kind == CXType_Complex ||
+           kind == CXType_Vector;
+}
+
+int is_c_primitive_type(enum CXTypeKind kind)
+{
+    return is_primitive_type(kind) &&
+           kind != CXType_Char16 &&
+           kind != CXType_Char32;
+}
+
+void format_c_primitive_type(enum CXTypeKind kind)
+{
+    static char * map[] = {
+        [CXType_Void] = "void",
+        [CXType_Bool] = "unsigned",
+        [CXType_Char_U] = "unsigned-char",
+        [CXType_UChar] = "unsigned-char",
+        [CXType_UShort] = "unsigned-short",
+        [CXType_UInt] = "unsigned",
+        [CXType_ULong] = "unsigned-long",
+        [CXType_ULongLong] = "unsigned-long-long",
+        [CXType_UInt128] = "unsigned-long-long-long",
+        
+    };
+    fprintf(ffifile, "(%s)", map[CXType_Void]);
+}
+
+void format_type_reference(CXCursor cursor, CXType type)
+{
+    enum CXTypeKind kind = type.kind;
+    CXString type_name = clang_getTypeKindSpelling(kind);
+
+    if (is_c_primitive_type(kind)) {
+        format_c_primitive_type(kind);
+        return;
+    }
+    
+    switch (kind) {
+        case CXType_Complex:
+            break;
+        case CXType_Pointer:
+            break;
+        case CXType_Record:
+            break;
+        case CXType_Enum:
+            break;
+        case CXType_Typedef:
+            break;
+        CXType_FunctionProto:
+            break;
+        case CXType_ConstantArray:
+            break;
+        case CXType_Vector:
+            break;
+        default:
+            fprintf(stderr, "Error: reference type %s not implemented.\n", clang_getCString(type_name));
+    }
+    clang_disposeString(type_name);
+}
+
+void process_var_decl(CXCursor cursor, CXString filename, unsigned line, CXString ident, CXType type)
+{
+    printf("(var (\"%s\" %u)\n", clang_getCString(filename), line);
+    printf("  \"%s\"\n", clang_getCString(ident));
+    printf("  ");
+    format_type_reference(cursor, type);
+    printf(" ");
+    format_storage_kind(cursor);
+    printf(")\n");
+}
+
 enum CXChildVisitResult visit_func(CXCursor cursor, CXCursor parent, CXClientData client_data)
 {
     CXSourceLocation location = clang_getCursorLocation(cursor);
@@ -180,9 +303,11 @@ enum CXChildVisitResult visit_func(CXCursor cursor, CXCursor parent, CXClientDat
     CXString filename = clang_getFileName(file);
     enum CXCursorKind cursor_kind = clang_getCursorKind(cursor);
     CXString ident = clang_getCursorSpelling(cursor);
-    CXString type = clang_getTypeSpelling(clang_getCursorType(cursor));
+    CXType type = clang_getCursorType(cursor);
 
     switch(cursor_kind) {
+        /* currently libclang has no way to access #undef,
+         * fortunately it's also ignored in parse-ffi.lisp */
         case CXCursor_MacroDefinition:
             process_macro_definition(cursor, filename, line);
             break;
@@ -192,14 +317,25 @@ enum CXChildVisitResult visit_func(CXCursor cursor, CXCursor parent, CXClientDat
         case CXCursor_EnumConstantDecl:
             process_enum_constant_decl(cursor, ident, *(int *)client_data);
             break;
+        case CXCursor_StructDecl:
+            break;
+        case CXCursor_UnionDecl:
+            break;
+        case CXCursor_FunctionDecl:
+            break;
+        case CXCursor_VarDecl:
+            process_var_decl(cursor, filename, line, ident, type);
+            break;
+        case CXCursor_TypedefDecl:
+            break;
+        default:
+            break;
     }
     // clang_visitChildren(cursor, visit_func, NULL);
     clang_disposeString(filename);
     clang_disposeString(ident);
-    clang_disposeString(type);
     return CXChildVisit_Continue;
 }
-
 
 void process_predefined_macro_definitions(CXIndex index)
 {
